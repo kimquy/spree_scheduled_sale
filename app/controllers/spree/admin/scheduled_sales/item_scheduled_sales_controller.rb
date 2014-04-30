821788f92ad990fecd0d07a9e83a3d59618b3428
Mike Porter <mike@agileedge.com>
2014-04-30 10:30:35 -0700

    integrated the creation of new ScheduledSalesItems and the dynamic update of the Sales Item table

--- app/controllers/spree/admin/scheduled_sales/item_scheduled_sales_controller.rb
+++ app/controllers/spree/admin/scheduled_sales/item_scheduled_sales_controller.rb
@@ -8,8 +8,16 @@ module Spree
         end
 
         def create
-          render :text => "YES"
+          @scheduled_sale = Spree::ScheduledSale.find params[:scheduled_sale_id]
+          @scheduled_sale.item_scheduled_sales.create!(item_scheduled_sales_params)
+
         end
+
+        :protected
+        def item_scheduled_sales_params
+          params.require(:item_scheduled_sale).permit(:item_id, :item_type)
+        end
+
       end
     end
   end
