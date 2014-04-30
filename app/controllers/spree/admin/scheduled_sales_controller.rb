8d8dab82f14cfd7ab6260dcb77b2c78a4a24f91f
Mike Porter <mike@agileedge.com>
2014-04-30 12:45:12 -0700

    derp derp.... made work for variants, products and taxons

--- app/controllers/spree/admin/scheduled_sales_controller.rb
+++ app/controllers/spree/admin/scheduled_sales_controller.rb
@@ -12,7 +12,7 @@ module Spree
 
       def items_search
         search = params[:item_type].constantize.search(:name_cont => params[:name])
-        render :json => search.result.map{|r| {id: r.id,  name: r.name_and_sku,  label: r.name_and_sku}}
+        render :json => search.result.map{|r| {id: r.id,   label: (r.respond_to?(:name_and_sku) ? r.name_and_sku : r.name)}}
       end
     end
   end
